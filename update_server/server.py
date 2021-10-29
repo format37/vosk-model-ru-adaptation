37a24158b6d01edcb3dd47ec587c83f81d8f158a
alex <format37@gmail.com>
2021-10-29 12:46:32 +0300

    modified:   update_server/server.py

--- update_server/server.py
+++ update_server/server.py
@@ -1,14 +1,15 @@
 from aiohttp import web
 import os
 import pandas as pd
+from io import StringIO
+import uuid
 
 async def call_test(request):
 	content = "ok"
 	return web.Response(text=content,content_type="text/html")
 
 
-# init
-async def call_read_words(request):
+async def call_download_dictionary(request):
 	words = pd.read_csv('model_files/words.txt', sep=" ")
 	words.columns = ['word', 'id']
 	words.drop(['id'], axis=1, inplace=True)
@@ -31,10 +32,19 @@ async def call_read_words(request):
 	return web.Response(text=response,content_type="text/html")
 
 
+async def call_upload_corpus(request):
+	csv_text = str(await request.text()).replace('\ufeff', '')
+	with open('model_files/corpus_batch_'+str(uuid.uuid4())+'.csv', 'wb') as f:
+		f.write(csv_text)
+
+	return web.Response(text='Upload successfull',content_type="text/html")
+
+
 def main():
 	app = web.Application(client_max_size=1024**3)
 	app.router.add_route('GET', '/test', call_test)
-	app.router.add_route('GET', '/read_words', call_read_words)
+	app.router.add_route('GET', '/download_dictionary', call_download_dictionary)
+	app.router.add_post('/upload_corpus', call_upload_corpus)
 
 	web.run_app(
 		app,
