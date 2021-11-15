ad363b6da2a7b38e31c089cbc1e694f7ce92512c
alex <format37@gmail.com>
2021-11-15 14:13:56 +0300

    modified:   server/Dockerfile 	modified:   update_server/download_words.sh

--- update_server/download_words.sh
+++ update_server/download_words.sh
@@ -1,2 +1,2 @@
-if [ -f "../model_files/words.txt" ]; then rm -r ../model_files/words.txt; fi 
-/usr/bin/docker cp vosk-model-ru-adaptation_server_1:/opt/vosk-model-ru/model/graph/words.txt ../model_files/
+#if [ -f "../model_files/words.txt" ]; then rm -r ../model_files/words.txt; fi 
+/usr/bin/docker cp vosk-model-ru-adaptation_server_z_1:/opt/vosk-model-ru/model/graph/words.txt ../model_files/
